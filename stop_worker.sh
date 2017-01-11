ae0e14dc45e3403b9c9dd056f4db724c6aa9ebeb
Rock de Vocht <peter@peter.co.nz>
2017-01-11 13:09:32 +1300

    added stop worker part 4

--- stop_worker.sh
+++ stop_worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PID=`ps xa | grep worker.py | grep -v grep | awk '{print $1;}'`
-[[ ! -z $PID ]] && echo "stopping $PID" && kill -TERM $PID
+[[ ! -z $PID ]] && echo "stopping $PID" && kill -TERM $PID && echo
