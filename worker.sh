08292c8e7023909b5355876c7aa395e50e83a1a8
Rock de Vocht <peter@peter.co.nz>
2017-01-11 14:11:17 +1300

    switched default to TEDLIUM

--- worker.sh
+++ worker.sh
@@ -5,4 +5,4 @@
 # test with:  gst-inspect-1.0 kaldinnet2onlinedecoder
 export GST_PLUGIN_PATH=/opt/kaldi/gst-plugin
 
-python kaldigstserver/worker.py -c recipes/librispeech_english_nnet2.yaml -u ws://localhost:8890/worker/ws/speech &
+python kaldigstserver/worker.py -c recipes/tedlium_english_nnet2.yaml -u ws://localhost:8890/worker/ws/speech &
